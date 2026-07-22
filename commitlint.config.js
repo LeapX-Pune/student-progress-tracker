6d014d46bdf57870bd56a184a2a415b031d01133
Kshitij Das <kshitij.das1501@gmail.com>
2026-07-22 08:28:46 +0530

    ci: add GitHub Actions CI/CD pipeline

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+    'scope-enum': [0],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'ci', 'revert', 'wip'],
+    ],
+  },
 };
