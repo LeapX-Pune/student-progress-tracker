f7fd5f3bf32627e247386823410710eea08d7e0d
Sauryaman Bisen <sauryamanbisen@gmail.com>
2026-07-23 12:32:34 +0530

    Update ModuleProgress.js

---
+++ src/components/courses/ModuleProgress.js
@@ -0,0 +1,5 @@
+export function createModuleProgress(completedModules, totalModules) {
+    // Deprecated by two-column details layout in CourseCard.js
+    // Keeping file to prevent import errors if used elsewhere
+    return document.createElement('div');
+}
